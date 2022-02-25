4fb2a74b0b8906cc481101d56e62ef3c27b45151
Efra <efrain_serna@hotmail.com>
2022-02-25 09:59:56 -0600

    AddressBook DB Context

---
+++ MovieRank/MovieRank.Libs/Models/AddressBookDb.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace MovieRank.Libs.Models
+{
+    [DynamoDBTable("PhoneContacts")]
+    public class AddressBookDb
+    {
+        [DynamoDBHashKey]
+        public int ContactId { get; set; }
+
+        [DynamoDBGlobalSecondaryIndexHashKey]
+        public string ContactName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Birthday { get; set; }
+    }
+}
